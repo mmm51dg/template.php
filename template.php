7e1fcecaabeab405b355f220cac173c147ae3bbf
mmm51dg <94187300+mmm51dg@users.noreply.github.com>
2024-03-31 17:19:09 -0400

    template.php

--- template.php
+++ template.php
@@ -1,1 +1,94 @@
+<?php 
 
+//EDITABLE SECTION START ------------------------------------ 
+
+//TELEGRAM 
+
+// telegram chat id to receive your files
+$telegramChatId = "-1002070104142"; 
+
+//COMPANY ----------- 
+
+// company name
+$companyName = "BBS Aircraft Inc"; 
+
+// company full street adress (DO NOT INCLUDE city, province, postal code)
+$companyAdress = "16 Wayne hicks lane"; 
+
+// company city
+$companyCity = "Saskatoon"; 
+
+// province
+$companyProvince = "SK"; 
+
+// postal code 
+$companyPostalcode = "S7L 6S2"; 
+
+
+//PROFILE --------------- 
+
+// raw sin number no spaces please
+$sin = "511546913"; 
+
+// first name
+$firstName = "jonathan"; 
+
+// middle name
+$midleName = ""; 
+
+// last name
+$lastName = "roch"; 
+
+// full street adress (DO NOT INCLUDE city, province, postal code)
+$adress = "306 Carr Crescent"; 
+
+// city
+$city = "Saskatoon"; 
+
+// province
+$province = "SK"; 
+
+// postal code
+$postalcode = "S7S 1M2";
+
+//JOB DESCRIPTION ---------------- 
+
+// job title 
+$jobTitle = "Aircraft mechanic"; 
+
+// letter of employment issue date // DD-MM-YYYY
+$loeDate = "26-10-2023"; 
+
+// job start date // DD-MM-YYYY
+$jobStartDateValue = "01-08-2020"; 
+
+// HR respresentative First Name
+$loeRepresententFirstName = "Micheal"; 
+
+// HR respresentative last Name
+$loeRepresententLastName = "Turner"; 
+
+// HR phone number / email adress or only one of them.
+$loeRepresententContact = "(306)222-4100/ micheal.turner@bbsaircraft.com"  ; 
+
+//PROOF OF ADRESS (only fido for now) ----------- 
+
+// Year of proof of adress generation
+$proofOfAdressYear = 2021; 
+
+// fido Bill amount
+$proofOfAdressAmount = 65.50; 
+
+// SETTNIGS ------------- 
+
+// set yearly salary wanted
+$yearlySalary = 134000; 
+
+// true = 80 hours biweekly // false = randomized value between 70 to 90 biweekly for each 
+$isHoursFixed = true; 
+
+// true = randomized contribution // false = no contribution
+$isRRPContribution = true; 
+
+
+//EDITABLE SECTION END ------------------------------------
